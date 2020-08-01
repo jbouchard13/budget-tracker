9f7509651a275ce32099a9618e883a6f5b303b09
John Bouchard <jbouchard13@gmail.com>
2020-08-01 16:28:20 -0400

    restructured files and started implementing webpack

--- routes/api.js
+++ routes/api.js
@@ -1,32 +1,36 @@
-const router = require("express").Router();
-const Transaction = require("../models/transaction.js");
+const router = require('express').Router();
+const Transaction = require('../models/transaction.js');
 
-router.post("/api/transaction", ({body}, res) => {
+// post route for creating new transactions
+router.post('/api/transaction', ({ body }, res) => {
   Transaction.create(body)
-    .then(dbTransaction => {
+    .then((dbTransaction) => {
       res.json(dbTransaction);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(404).json(err);
     });
 });
 
-router.post("/api/transaction/bulk", ({body}, res) => {
+// post route for adding all offline transactions to db
+router.post('/api/transaction/bulk', ({ body }, res) => {
   Transaction.insertMany(body)
-    .then(dbTransaction => {
+    .then((dbTransaction) => {
       res.json(dbTransaction);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(404).json(err);
     });
 });
 
-router.get("/api/transaction", (req, res) => {
-  Transaction.find({}).sort({date: -1})
-    .then(dbTransaction => {
+// get route to retrieve all transaction info
+router.get('/api/transaction', (req, res) => {
+  Transaction.find({})
+    .sort({ date: -1 })
+    .then((dbTransaction) => {
       res.json(dbTransaction);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(404).json(err);
     });
 });
